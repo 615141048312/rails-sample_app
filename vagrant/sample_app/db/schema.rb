49a5981255cfb15fa8725055b546f6b863bfdbfa
花田　結衣 <yui@hanadayuinoMBP.lan>
2020-04-16 21:17:48 +0900

    [finish] 3-5

--- vagrant/sample_app/db/schema.rb
+++ vagrant/sample_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_085527) do
+ActiveRecord::Schema.define(version: 2020_04_16_111123) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
