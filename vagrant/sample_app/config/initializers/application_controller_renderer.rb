cd299920fbb44e6b84ce16439a59e7bcd8667ef8
花田　結衣 <yui@hanadayuinoMBP.lan>
2020-04-18 15:18:31 +0900

    [start] 3-9

--- vagrant/sample_app/config/initializers/application_controller_renderer.rb
+++ vagrant/sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b9308f2f150a03029c348dc3460637fd007ecb9ab69cd83abd3239c3269dd4b307504f83457bf5b2f4a77f18a21e21a86cfc14b26f67db96f648ecb5e9ac5d88'
